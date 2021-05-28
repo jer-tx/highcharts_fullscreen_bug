e32003a64d5dc4df29267b6e0ad75bd4d1b25b0f
Jeremy <jdavis.ini@gmail.com>
2021-05-28 13:09:04 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HighchartsChartModule} from "highcharts-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'serverApp'}),
+        HighchartsChartModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
